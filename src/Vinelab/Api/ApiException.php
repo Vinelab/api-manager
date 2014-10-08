bb44971cd74f37d9962af049feeedc1d602c3fad
mahmoud <inbox@mahmoudzalt.com>
2014-10-08 17:09:51 +0300

    add API exception

---
+++ src/Vinelab/Api/ApiException.php
@@ -0,0 +1,9 @@
+<?php namespace Vinelab\Api;
+/**
+ * @author Mahmoud Zalt <mahmoud@vinelab.com>
+ */ 
+class ApiException extends \Exception{
+
+
+
+}
--- src/Vinelab/Api/ErrorHandler.php
+++ src/Vinelab/Api/ErrorHandler.php
@@ -3,26 +3,29 @@
  * @author Mahmoud Zalt <mahmoud@vinelab.com>
  */
 use Illuminate\Support\Facades\Response;
+use Vinelab\Api\ApiException;
 
 class ErrorHandler {
 
     public function handle($exception, $code = 0, $status = 500, $headers = [], $options = 0)
     {
-        $message = $exception;
 
-        // If the exception is on of ours then treat it as implemented.
-        if ($exception instanceof ApiException)
+        if (is_string($exception))
         {
-            $code = $exception->getCode();
-            $message = $exception->getMessages($code, $status);
+            $message = $exception;
         }
         // This is a generic, non-supported exception so we'll just treat it as so.
         elseif ($exception instanceof Exception or $exception instanceof RuntimeException)
         {
             $code = $exception->getCode();
             $message = $exception->getMessage();
-        }else{
-            $message = null;
+        }
+        // if not Exception or RuntimeException or a string then throw and API exception
+        else
+        {
+            throw new ApiException('handle() first parameter expects an instance of Exception or
+                                    RuntimeException or a string, ' . get_class($exception) . ' is
+                                    given.');
         }
 
         return Response::json([
